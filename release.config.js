b642f04cb822db88c259f8b29f0877b29ab8b3d9
Marc Easen <marc@easen.co.uk>
2021-11-11 16:27:33 +0000

    chore: release config to create github releases

--- release.config.js
+++ release.config.js
@@ -2,8 +2,9 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/github',
+    ['@semantic-release/github', { publish : true}],
     [
       '@semantic-release/git',
       {
